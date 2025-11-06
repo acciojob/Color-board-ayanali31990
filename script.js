f27310e7103e7a6c9b9a2f48bf3ee733753ff883
Ayan Ali <149358183+ayanali31990@users.noreply.github.com>
2025-11-06 10:35:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
 //your JS code here. If required.
+const container = document.querySelector('.container');
+
+const boxCount = 800;
+
+for(let i = 0; i < boxCount; i++){
+    const box = document.createElement('div');
+    box.classList.add('square');
+
+    box.addEventListener('mouseover', () => {
+        const randomColor = getRandomColor();
+        box.style.background = randomColor;
+
+        setTimeout(() => {
+            box.style.background = '#222';
+        }, 1000);
+    });
+
+    container.appendChild(box);
+}
+
+function getRandomColor(){
+    const colors = [
+        '#e74c3c', '#8e44ad', '#3498db', '#e67e22',
+        '#2ecc71', '#1abc9c', '#f1c40f', '#d35400'
+    ];
+    return colors[Math.floor(Math.random() * colors.length)];
+}
